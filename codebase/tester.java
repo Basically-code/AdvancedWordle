1ee46946b52797ab0d21c604e6d0fe98d3ae747b
g26k8977 <hansjkimaro@gmail.com>
2026-09-14 21:03:56 +0200

    On progress

---
+++ codebase/tester.java
@@ -0,0 +1,11 @@
+
+
+public  class tester {
+
+    public static void main(String[] args){
+        methods method = new methods();
+        //method.range("data/words_alpha.txt","data/miniWords.txt",4,7);
+        gameLogic game = new gameLogic();
+        game.runGame();
+    }
+}
